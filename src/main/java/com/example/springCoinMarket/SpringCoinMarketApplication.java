f1731bb2537615555c92580e24003a23e86ce5a0
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-02-22 20:33:55 +0300

    init commit

---
+++ src/main/java/com/example/springCoinMarket/SpringCoinMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.springCoinMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoinMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoinMarketApplication.class, args);
+	}
+
+}
