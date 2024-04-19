70828cfac9c6b7c41932ac864511088a7275bb2a
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-04-19 11:26:40 +0300

    add controller for crypto currency

---
+++ src/main/java/com/example/springCoinMarket/controller/CryptocurrencyController.java
@@ -0,0 +1,26 @@
+package com.example.springCoinMarket.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CryptocurrencyController {
+    private String url = "https://api.binance.com/api/v3/ticker/price?symbol=";
+
+    @GetMapping("/crypto/price/{symbol}")
+    public String getPrice(@PathVariable String symbol) {
+        String newUrl = url + symbol;
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(newUrl, String.class);
+
+        return response;
+    }
+
+    @GetMapping("/crypto")
+    public String[] getCrypto()
+    {
+        return new String[]{"BTCUSDT", "ETHUSDT", "BNBUSDT"};
+    }
+}
